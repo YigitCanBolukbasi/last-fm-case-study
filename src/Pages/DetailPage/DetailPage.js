754d5a5a57ecc211cdd7fd2e8c5de3918bef9439
Yigit Can <y.can.bolukbasi@gmail.com>
2022-03-19 01:10:23 +0300

    main structure created

---
+++ src/Pages/DetailPage/DetailPage.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import styles from './DetailPage.styles';
+
+const DetailPage = () => {
+  return (
+    <View>
+      <Text>DetailPage</Text>
+    </View>
+  );
+};
+
+export default DetailPage;
---
+++ src/Pages/MainPage/MainPage.js
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+import styles from './MainPage.styles';
+
+const MainPage = () => {
+  return (
+    <View>
+      <Text>MainPage</Text>
+    </View>
+  );
+};
+
+export default MainPage;
