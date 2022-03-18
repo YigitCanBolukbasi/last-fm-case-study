140198a5b6a97ca6f9777fd0b200c8355bc021d8
Yigit Can <y.can.bolukbasi@gmail.com>
2022-03-19 01:17:09 +0300

    cards stracture implemented

---
+++ src/Components/Cards/ArtistCard/ArtistCard.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import styles from './ArtistCard.styles';
+
+const ArtistCard = () => {
+  return (
+    <View>
+      <Text>ArtistCard</Text>
+    </View>
+  );
+};
+
+export default ArtistCard;
---
+++ src/Components/Cards/ProductCard/ProductCard.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import styles from './ProductCard.styles';
+
+const ProductCard = () => {
+  return (
+    <View>
+      <Text>ProductCard</Text>
+    </View>
+  );
+};
+
+export default ProductCard;
