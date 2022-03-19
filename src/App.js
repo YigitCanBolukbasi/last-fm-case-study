7ac3813135430eb053d22b3531ff90713eff8f17
Yigit Can <y.can.bolukbasi@gmail.com>
2022-03-19 10:48:18 +0300

    Navigation page created

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@
 
 import * as React from 'react';
 import {View, Text} from 'react-native';
+
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-function HomeScreen() {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import MainPage from './Pages/MainPage/MainPage';
+import DetailPage from './Pages/DetailPage/DetailPage';
 
 const Stack = createNativeStackNavigator();
 
@@ -19,7 +15,8 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="HomeScreen" component={MainPage} />
+        <Stack.Screen name="DetailScreen" component={DetailPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
