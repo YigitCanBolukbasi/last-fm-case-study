cc3190bb0a992b8221cb4f7e619e0466c1656125
Yigit Can <y.can.bolukbasi@gmail.com>
2022-03-20 20:41:18 +0300

    Detail page styles implemented

--- src/Pages/DetailPage/DetailPage.styles.js
+++ src/Pages/DetailPage/DetailPage.styles.js
@@ -3,13 +3,16 @@ import {StyleSheet, Dimensions} from 'react-native';
 export default StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'white',
+  },
+  inner_container: {
+    flex: 1,
     flexDirection: 'row',
     backgroundColor: 'white',
-    marginTop: 30,
+    borderTopWidth: 2,
   },
   albums_container: {
     width: Dimensions.get('window').width / 2,
-    marginTop: 40,
   },
   track_container: {
     width: Dimensions.get('window').width / 2,
@@ -19,5 +22,33 @@ export default StyleSheet.create({
     color: 'black',
     borderBottomWidth: 1,
     borderBottomColor: 'black',
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  artist_card: {
+    flex: 1,
+    padding: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 12,
+  },
+  artist_shadow: {
+    height: Dimensions.get('window').height / 10,
+    margin: 10,
+    padding: 6,
+    borderRadius: 10,
+    elevation: 20,
+  },
+  artist_image: {
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    margin: 10,
+  },
+  artist_text: {
+    fontSize: 18,
+    fontWeight: '800',
+    color: '#000',
   },
 });
